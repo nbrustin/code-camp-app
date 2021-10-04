f442d5b7caa736965dc278c168586f3b1304e5ea
Nick Brustin <nbrustin@gmail.com>
2021-10-04 12:04:46 -0700

    Update Header.js

--- Src/components/Header.js
+++ Src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
       <div className="container mobile-container">
         <div className="d-flex justify-content-between">
           <div>
-            <img alt="SVCC Home Page" src="/images/SVCCLogo.png"></img>
+            <img alt="SVCC Home Page" src="/images/SVCClogo.png"></img>
           </div>
           <div className="light">
             <h4 className="header-title">Silicon Valley Code Camp</h4>
